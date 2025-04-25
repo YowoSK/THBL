6b04aac3e6efd24cacc9959ceba48865c93db467
Yowo <83427860+YowoSK@users.noreply.github.com>
2025-04-25 02:01:09 +0200

    project init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
+import { StatsComponent } from './stats/stats.component';
+import { PlayersComponent } from './players/players.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'teams', component: TeamsComponent },
+  { path: 'stats', component: StatsComponent },
+  { path: 'players', component: PlayersComponent },
+];
