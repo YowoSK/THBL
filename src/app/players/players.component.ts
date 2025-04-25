d0fc388724e8e6848af5a7e2ff7e8022cca05284
Yowo <83427860+YowoSK@users.noreply.github.com>
2025-04-25 03:04:22 +0200

    updated

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import jsonData from '../../assets/data/players.json'
 
 @Component({
   selector: 'app-players',
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './players.component.html',
   styleUrl: './players.component.css'
 })
 export class PlayersComponent {
+  players: any[] = jsonData.players;
 
 }
